212b0bfb9ce2c2354474c29fab887599fc0d2d68
Jake Teton-Landis <just.1.jake@gmail.com>
2020-01-05 10:44:59 -0800

    wip: start implementing the figma post

--- ts/quickjs.ts
+++ ts/quickjs.ts
@@ -1,20 +1,130 @@
 import QuickJSModuleLoader from '../build/wrapper/wasm/quickjs-emscripten-module'
+import { LowLevelJavascriptVm, VmHandle } from './types'
 
 const QuickJSModule = QuickJSModuleLoader()
 const initialized = new Promise(resolve => { QuickJSModule.onRuntimeInitialized = resolve })
 
-type SuccessOrFail<T> = {
-  value: T
-  error?: undefined
-} | {
-  error: string | object | number
-}
+const pointerBrand = Symbol('pointer')
+
+
+
+// FFI types
+type Pointer<Brand extends string> = number & { [pointerBrand]: Brand }
+type RuntimePointer = Pointer<'JSRuntime'>
+type ContextPointer = Pointer<'JSContext'>
+type ValuePointer = Pointer<'JSValue'>
 
 /**
  * QuickJSFFI contains `cwrap` declarations that expose C functions from the Emscripten module.
+ * @private
  */
 class QuickJSFFI {
+  newRuntime: () => RuntimePointer = QuickJSModule.cwrap('JS_NewRuntime', 'number', [])
+  freeRuntime: (rt: RuntimePointer) => void = QuickJSModule.cwrap('JS_FreeRuntime', null, ['number'])
+
+  newContext: (rt: RuntimePointer) => ContextPointer = QuickJSModule.cwrap('JS_NewContext', 'number', ['number'])
+  freeContext: (ctx: ContextPointer) => void = QuickJSModule.cwrap('JS_FreeContext', null, ['number'])
+
+  // TODO: implement
+  freeJSValuePointer: (ctx: ContextPointer, value: ValuePointer) => void = QuickJSModule.cwrap('WP_FreeJSValuePointer', null, ['number', 'number'])
+
   eval: (code: string) => string = QuickJSModule.cwrap('eval', 'string', ['string'])
+}
+
+class Freeable<T> {
+  private _freed: boolean = false
+  constructor(
+    private readonly _value: T,
+    private readonly freeImpl?: (value: T) => void
+  ) {}
+
+  get freed() { return this._freed }
+  free() {
+    this.assertAlive()
+    if (this.freeImpl) {
+      this.freeImpl(this._value)
+    }
+    this._freed = true
+  }
+  get value() {
+    this.assertAlive()
+    return this._value
+  }
+  assertAlive() {
+    if (this._freed) {
+      throw new Error('Already freed')
+    }
+  }
+}
+
+class QuickJSVm implements LowLevelJavascriptVm<QuickJSValueHandle> {
+  private readonly ffi: QuickJSFFI
+  private readonly ctx: Freeable<ContextPointer>
+  private readonly rt: Freeable<RuntimePointer>
+
+  constructor(args: {
+    ffi: QuickJSFFI
+    ctx: ContextPointer
+    rt: RuntimePointer
+  }) {
+    this.ffi = args.ffi
+    this.ctx = new Freeable(args.ctx, ctx => this.ffi.freeContext(ctx))
+    this.rt = new Freeable(args.rt, rt => this.ffi.freeRuntime(rt))
+  }
+
+  // interface
+
+  typeof(handle: QuickJSValueHandle) {
+    // no need to check owner
+    return this.ffi.typeof(handle.pointer.value)
+  }
+
+  getNumber(handle: QuickJSValueHandle) {
+    this.assertOwned(handle)
+    return this.ffi.toFloat64(this.ctx.value, handle.pointer.value)
+  }
+
+  getString(handle: QuickJSValueHandle) {
+    this.assertOwned(handle)
+    return this.ffi.toString(this.ctx.value, handle.pointer.value)
+  }
+
+  // TODO: finish implementation
+
+  // customizations
+
+  free() {
+    this.ctx.free()
+    this.rt.free()
+  }
+
+  freeHandle(handle: QuickJSValueHandle) {
+    this.assertOwned(handle)
+    this.ffi.freeJSValuePointer(this.ctx.value, handle.pointer.value)
+  }
+
+  assertOwned(handle: QuickJSValueHandle) {
+    if (handle.vm !== this) {
+      throw new Error('Given handle created by a different VM')
+    }
+  }
+}
+
+/**
+ * QuickJSHandle wraps a pointer to a QuickJS JSValue
+ */
+class QuickJSValueHandle implements VmHandle {
+  readonly vm: QuickJSVm
+  readonly pointer: Freeable<ValuePointer>
+
+  constructor(vm: QuickJSVm, pointer: ValuePointer) {
+    this.vm = vm
+    this.pointer = new Freeable(pointer, () => this.vm.freeHandle(this))
+  }
+
+  free() {
+    this.pointer.free()
+  }
 }
 
 /**
@@ -24,19 +134,14 @@ class QuickJSFFI {
 class QuickJS {
   private ffi = new QuickJSFFI()
 
-  /**
-   * Evaluate a string of Javascript code, returning its result.
-   *
-   * Note that the result is encoded with JSON, so a snippet that produces a cycle
-   * will throw an exception inside QuickJS.
-   */
-  eval(code: string): SuccessOrFail<any> {
-    const result = this.ffi.eval(code)
-    try {
-      return JSON.parse(result)
-    } catch(error) {
-      return { error: result }
-    }
+  createVm(): QuickJSVm {
+    const rtPointer = this.ffi.newRuntime()
+    const ctxPointer = this.ffi.newContext(rtPointer)
+    return new QuickJSVm({
+      ffi: this.ffi,
+      rt: rtPointer,
+      ctx: ctxPointer,
+    })
   }
 }
 
---
+++ ts/types.ts
@@ -0,0 +1,43 @@
+export type SuccessOrFail<S, F> = {
+  value: S
+  error?: undefined
+} | {
+  error: F
+}
+
+export type VmCallResult<VmHandle> = SuccessOrFail<VmHandle, VmHandle>
+
+/**
+ * From https://www.figma.com/blog/how-we-built-the-figma-plugin-system/
+ */
+export interface LowLevelJavascriptVm<VmHandle> {
+  undefined: VmHandle
+
+  typeof(handle: VmHandle): string
+
+  getNumber(handle: VmHandle): number
+  getString(handle: VmHandle): string
+
+  newNumber(value: number): VmHandle
+  newString(value: string): VmHandle
+  newObject(prototype?: VmHandle): VmHandle
+  newFunction(name: string, value: (this: VmHandle, ...args: VmHandle[]) => VmHandle): VmHandle
+
+  // For accessing properties of objects
+  getProp(handle: VmHandle, key: string | VmHandle): VmHandle
+  setProp(handle: VmHandle, key: string | VmHandle, value: VmHandle): void
+  defineProp(handle: VmHandle, key: string | VmHandle, descriptor: VmPropertyDescriptor<VmHandle>): void
+
+  callFunction(func: VmHandle, thisVal: VmHandle, ...args: VmHandle[]): VmCallResult<VmHandle>
+  evalCode(code: string): VmCallResult<VmHandle>
+}
+
+/**
+ * From https://www.figma.com/blog/how-we-built-the-figma-plugin-system/
+ */
+export interface VmPropertyDescriptor<VmHandle> {
+  configurable?: boolean
+  enumerable?: boolean
+  get?: (this: VmHandle) => VmHandle
+  set?: (this: VmHandle, value: VmHandle) => void
+}
