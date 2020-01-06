f249c452770df2577b1f7ee1e0c18e591b2981cf
Jake Teton-Landis <just.1.jake@gmail.com>
2020-01-06 09:38:56 -0800

    fix segfault (no free after SetProperty)

--- c/interface.c
+++ c/interface.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <math.h>  // For NAN
 #include "../quickjs/quickjs.h"
 #include "../quickjs/quickjs-libc.h"
 
@@ -30,6 +31,20 @@ void qjs_dump(JSContext *ctx, JSValueConst value) {
  * type SuccessOrFail<T> = { result: T } | { error: unknown }
  */
 
+void copy_prop_if_needed(JSContext* ctx, JSValueConst dest, JSValueConst src, const char* prop_name) {
+  JSAtom prop_atom = JS_NewAtom(ctx, prop_name);
+  JSValue dest_prop = JS_GetProperty(ctx, dest, prop_atom);
+  if (JS_IsUndefined(dest_prop)) {
+    JSValue src_prop = JS_GetProperty(ctx, src, prop_atom);
+    if (!JS_IsUndefined(src_prop) && !JS_IsException(src_prop)) {
+      JS_SetProperty(ctx, dest, prop_atom, src_prop);
+    }
+  } else {
+    JS_FreeValue(ctx, dest_prop);
+  }
+  JS_FreeAtom(ctx, prop_atom);
+}
+
 
 JSValue wrap_error(JSContext *ctx, JSValueConst error) {
   const char* error_prop = "error";
@@ -37,8 +52,6 @@ JSValue wrap_error(JSContext *ctx, JSValueConst error) {
 
   // Try #1: { ...error, stack, message }
   // Clone via JSON.parse(JSON.stringify(error))
-  JSValue stack = JS_GetPropertyStr(ctx, error, "stack");
-  JSValue message = JS_GetPropertyStr(ctx, error, "message");
   JSValue error_json = JS_JSONStringify(ctx, error, JS_UNDEFINED, JS_UNDEFINED);
   if (!JS_IsException(error_json)) {
     QJS_DEBUG("converted error to json");
@@ -48,59 +61,45 @@ JSValue wrap_error(JSContext *ctx, JSValueConst error) {
       JSValue error_props = JS_ParseJSON(ctx, error_json_cstring, strlen(error_json_cstring), error_prop);
       JS_FreeCString(ctx, error_json_cstring);
       if (!JS_IsException(error_props)) {
-        // Set possibly non-enumerable props stack and message on error
-        if (!JS_IsException(stack) && !JS_IsUndefined(stack)) {
-          QJS_DEBUG("setting stack");
-          JS_SetPropertyStr(ctx, error_props, "stack", stack);
-          /*JS_FreeValue(ctx, stack);*/
-          QJS_DEBUG("done: setting stack");
-        }
-        if (!JS_IsException(message) && !JS_IsUndefined(message)) {
-          QJS_DEBUG("setting message");
-          JS_SetPropertyStr(ctx, error_props, "message", message);
-          /*JS_FreeValue(ctx, message);*/
-          QJS_DEBUG("done: setting message");
-        }
+        // Fill in error properties if they exist
+        copy_prop_if_needed(ctx, error_props, error, "name");
+        copy_prop_if_needed(ctx, error_props, error, "message");
+        copy_prop_if_needed(ctx, error_props, error, "stack");
 
         JS_SetPropertyStr(ctx, result, error_prop, error_props);
-        JS_FreeValue(ctx, error_props);
+        // no FreeValue: SetProperty consumes the value
         QJS_DEBUG("set { error: error json }");
-
         return result;
       }
     }
   }
-  // Clean up stack & message if we had an error
-  JS_FreeValue(ctx, stack);
-  JS_FreeValue(ctx, message);
-  QJS_DEBUG("freed stack and message")
 
   // Try #2: Error to string
   JSValue error_as_string = JS_ToString(ctx, error);
   if (!JS_IsException(error_as_string)) {
     QJS_DEBUG("converted error to string")
     JS_SetPropertyStr(ctx, result, error_prop, error_as_string);
-    /*JS_FreeValue(ctx, error_as_string);*/
+    // no FreeValue: SetProperty consumes the value
     return result;
   }
 
   // Last resort: set to a fixed error string.
-  QJS_DEBUG("last resort")
+  QJS_DEBUG("Failed to convert error to string:")
   js_std_dump_error(ctx);
   JSValue best_we_could_do = JS_NewString(ctx, QJS_TYPESCRIPT "failed to create error");
   JS_SetPropertyStr(ctx, result, error_prop, best_we_could_do);
-  /*JS_FreeValue(ctx, best_we_could_do);*/
+  // no FreeValue: SetProperty consumes the value
   return result;
 }
 
-JSValue wrap_success(JSContext *ctx, JSValueConst success) {
+JSValue wrap_success(JSContext *ctx, JSValue success) {
   const char* success_prop = "value";
   JSValue result = JS_NewObject(ctx);
   JS_SetPropertyStr(ctx, result, success_prop, success);
   return result;
 }
 
-char* to_result_json(JSContext* ctx, JSValueConst result, int handle_json_error) {
+char* to_result_json(JSContext* ctx, JSValue result, int handle_json_error) {
   JSValue wrapped;
   if (JS_IsException(result)) {
     QJS_DEBUG("handling exception")
@@ -110,10 +109,12 @@ char* to_result_json(JSContext* ctx, JSValueConst result, int handle_json_error)
     QJS_DEBUG("wrapped error");
     QJS_DEBUG("wrapped error, now freeing");
     JS_FreeValue(ctx, error);
+    JS_FreeValue(ctx, result);
     QJS_DEBUG("freed error");
     /*QJS_DUMP(wrapped);*/
   } else {
     wrapped = wrap_success(ctx, result);
+    // wrap_success consumes result;no free
     QJS_DEBUG("wrapped success")
   }
 
@@ -159,10 +160,10 @@ char* eval(char* js_code) {
   // Do stuff
   JSValue value = JS_Eval(ctx, js_code, strlen(js_code), "eval.js", 0);
   result = to_result_json(ctx, value, 1);
+  // value consumed by to_result_json
   QJS_DEBUG("got a result");
   /*QJS_DEBUG(result);*/
   /*QJS_DUMP(value);*/
-  /*JS_FreeValue(ctx, value);*/
 
   // Free JS stuff
   JS_FreeContext(ctx);
@@ -177,7 +178,7 @@ char* eval(char* js_code) {
  * FFI
  */
 
-JSValue *jsvalue_to_heap(JSValue value) {
+JSValue *jsvalue_to_heap(JSValueConst value) {
   JSValue* result = malloc(sizeof(JSValue));
   if  (result) {
     memcpy(result, &value, sizeof(JSValue));
@@ -199,7 +200,7 @@ JSValue *QTS_NewFloat64(JSContext *ctx, double num) {
 }
 
 double QTS_GetFloat64(JSContext *ctx, JSValue *value) {
-  double result = 0;
+  double result = NAN;
   JS_ToFloat64(ctx, &result, *value);
   return result;
 }
@@ -217,14 +218,16 @@ char* QTS_GetString(JSContext *ctx, JSValue *value) {
 
 char* QTS_Typeof(JSContext *ctx, JSValue *value) {
   const char* result = "";
-  if (JS_IsString(*value)) {
-    result = "string";
-  } else if (JS_IsNumber(*value)) {
-    result = "number";
-  } else if (JS_IsBool(*value)) {
-    result = "boolean";
-  }
-  // TODO: finish
+
+  if (JS_IsNumber(*value)) { result = "number"; }
+  else if (JS_IsBool(*value)) { result = "boolean"; }
+  else if (JS_IsNull(*value)) { result = "object"; }
+  else if (JS_IsUndefined(*value)) { result = "undefined"; }
+  else if (JS_IsUninitialized(*value)) { result = "undefined"; }
+  else if (JS_IsString(*value)) { result = "string"; }
+  else if (JS_IsSymbol(*value)) { result = "symbol"; }
+  else if (JS_IsObject(*value)) { result = "object"; }
+  else if (JS_IsFunction(ctx, *value)) { result = "function"; }
 
   char* out = malloc(sizeof(char) * strlen(result));
   strcpy(out, result);
