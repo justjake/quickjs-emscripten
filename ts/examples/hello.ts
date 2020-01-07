a0f0e5dbbdf53be95e4c9e8c1757ec9b6b9a0dff
Jake Teton-Landis <just.1.jake@gmail.com>
2020-01-07 08:40:39 -0800

    fix c exports

--- ts/examples/hello.ts
+++ ts/examples/hello.ts
@@ -2,8 +2,12 @@ import {getInstance} from '../quickjs'
 
 async function main() {
   const quickjs = await getInstance()
-  const result = quickjs.eval('"hello " + "world" + !"')
-  console.log(result)
+  const vm = quickjs.createVm()
+  const num = vm.newNumber(55)
+  console.log('num', vm.getNumber(num))
+  const str = vm.newString('hi nora')
+  console.log('str', vm.getString(str))
+  vm.free()
 }
 
 main()
