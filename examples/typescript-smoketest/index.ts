501978bca108277b89ffe7a78f8b9840e5415bdf
Jake Teton-Landis <just.1.jake@gmail.com>
2020-06-27 17:55:12 -0700

    add smoketest to repo

---
+++ examples/typescript-smoketest/index.ts
@@ -0,0 +1,6 @@
+import { getQuickJS } from "quickjs-emscripten";
+async function main() {
+  const QuickJS = await getQuickJS();
+  console.log(QuickJS.evalCode('"Smoketest successful"'));
+}
+main();
