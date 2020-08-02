f769bd8b2abd24fbcb0f582e3950897f10094f81
Jake Teton-Landis <just.1.jake@gmail.com>
2020-08-01 23:22:39 -0700

    node smoketest: show memory usage

--- examples/typescript-smoketest/index.ts
+++ examples/typescript-smoketest/index.ts
@@ -1,6 +1,10 @@
 import { getQuickJS } from 'quickjs-emscripten'
 async function main() {
   const QuickJS = await getQuickJS()
-  console.log(QuickJS.evalCode('"Smoketest successful"'))
+  console.log(QuickJS.evalCode('"QuickJS.evalCode successful"'))
+
+  const vm = QuickJS.createVm()
+  const memoryUsage = vm.dumpMemoryUsage()
+  console.log('Empty VM memory usage:', memoryUsage)
 }
 main()
