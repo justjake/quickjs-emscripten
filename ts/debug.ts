84f20e48407ee27361bbbd74faa81dc383518d3c
Lukas Bickel <lukas.bickel@consolidate.at>
2022-06-13 14:53:34 +0200

    fix reference error in browser
    closes #65

--- ts/debug.ts
+++ ts/debug.ts
@@ -1,2 +1,2 @@
-export const QTS_DEBUG = false || Boolean(process.env.QTS_DEBUG)
+export const QTS_DEBUG = false || Boolean(typeof process === "object" && process.env.QTS_DEBUG)
 export let debugLog = QTS_DEBUG ? console.log.bind(console) : () => {}
