8b348b7e0969778e359b50566110b601eed4fb86
Jake Teton-Landis <just.1.jake@gmail.com>
2023-02-18 18:07:30 -0800

    awkward async import issue

---
+++ ts/esmHelpers.ts
@@ -0,0 +1,16 @@
+/** Typescript thinks import('...js/.d.ts') needs mod.default.default */
+function fakeUnwrapDefault<T>(mod: { default: T }): T {
+  // console.log("fakeUnwrapDefault", mod)
+  return mod as T
+}
+
+/** Typescript thinks import('...ts') doesn't need mod.default.default, but does */
+function actualUnwrapDefault<T>(mod: T): T {
+  // console.log("actualUnwrapDefault", mod)
+  return (mod as any).default
+}
+
+// I'm not sure if this behavior is needed in all runtimes,
+// or just for mocha + ts-node.
+export const unwrapTypescript = actualUnwrapDefault
+export const unwrapJavascript = fakeUnwrapDefault
