e167d737ac2dbc089e8f856568272192774b10cf
Jake Teton-Landis <just.1.jake@gmail.com>
2023-12-26 21:35:53 -0800

    prettier, linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 /* eslint-env node */
 /* @type import("eslint").ESLintConfig */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   root: true,
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     // Covered by prettier
-    "no-extra-semi": "off"
-  }
-};
+    "no-extra-semi": "off",
+  },
+}
