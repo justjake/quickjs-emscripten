33c671db2d9f17b196df904b7d86261377649efe
Jake Teton-Landis <just.1.jake@gmail.com>
2024-01-24 15:54:56 -0500

    Polyfill Symbol.dispose

--- packages/quickjs-emscripten-core/src/lifetime.ts
+++ packages/quickjs-emscripten-core/src/lifetime.ts
@@ -4,9 +4,6 @@ import { QTS_DEBUG } from "./debug"
 import { QuickJSUseAfterFree } from "./errors"
 import type { QuickJSHandle } from "./types"
 
-// Note: we don't need to polyfill Symbol.dispose because ESBuild falls back to
-// Symbol.for("Symbol.dispose") when Symbol.dispose is not available.
-
 /**
  * An object that can be disposed.
  * {@link Lifetime} is the canonical implementation of Disposable.
@@ -49,6 +46,14 @@ export abstract class UsingDisposable implements Disposable {
   [Symbol.dispose]() {
     return this.dispose()
   }
+}
+
+// Polyfill as needed if Symbol.dispose is not available.
+// This polyfill matches ESBuild's behavior.
+const SymbolDispose = Symbol.dispose ?? Symbol.for("Symbol.dispose")
+const prototypeAsAny = UsingDisposable.prototype as any
+if (!prototypeAsAny[SymbolDispose]) {
+  prototypeAsAny[SymbolDispose] = function () { return this.dispose() }
 }
 
 /**
